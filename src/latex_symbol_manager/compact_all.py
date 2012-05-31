2a46a73e98e4657ace83b00433d64a7fad3e515a
Andrea Censi <andrea@cds.caltech.edu>
2012-05-30 18:53:46 -0700

    done

--- src/latex_symbol_manager/compact_all.py
+++ src/latex_symbol_manager/compact_all.py
@@ -10,24 +10,52 @@ def main():
     parser.add_option("--highlight", help="Highlight use of known symbols",
                         default=False, action='store_true')
 
+    parser.add_option("--deprecated", help="Highlight use of deprecated",
+                        default=False, action='store_true')
+
+    parser.add_option("--markfirst", help="Mark first command",
+                        default=False, action='store_true')
+
     (options, args) = parser.parse_args() #@UnusedVariable
+
+    if options.markfirst:
+        sys.stdout.write("""
+           %\\newcommand{\\markfirst}[3]{#3}
+        """)
 
     for el in parse_all_symbols(args):
         if isinstance(el, OtherLine):
             sys.stdout.write(el.line)
             sys.stdout.write('\n')
         elif isinstance(el, Symbol):
+            filters = []
+
             if options.highlight:
                 if el.nargs == 0:
-                    wrapper = lambda x: "{\\color[rgb]{0,0.5,0} %s}" % x
-                else:
-                    wrapper = lambda x: x
-            else:
-                wrapper = lambda x: x
+                    def highlight(x):
+                        return "{\\color[rgb]{0,0.5,0} %s}" % x
+                    filters.append(highlight)
 
-            if 'deprecated' in el.other and el.nargs == 0:
-                wrapper = lambda x: "{\\color[rgb]{1,0,0} %s}" % x
+            if options.deprecated:
+                if 'deprecated' in el.other and el.nargs == 0:
+                    def mark_deprecated(x):
+                        return "{\\color[rgb]{1,0,0} %s}" % x
+                    filters.append(highlight)
 
+            if options.markfirst:
+                if el.nargs == 0:
+                    boolname = 'used%s' % str(el.symbol[1:])
+                    def mark_first(x):
+                        return "\\markfirst{%s}{%s}{%s}" % (el.symbol[1:], boolname, x)
+                    filters.append(mark_first)
+
+                    sys.stdout.write('\\newbool{%s}\\setbool{%s}{false}\n'
+                                     % (boolname, boolname))
+
+            def wrapper(x):
+                for f in filters:
+                    x = f(x)
+                return x
             sys.stdout.write(el.tex_definition(wrapper=wrapper))
             sys.stdout.write('\n')
         elif isinstance(el, SymbolSection):
