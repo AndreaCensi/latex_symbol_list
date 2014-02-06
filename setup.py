3929f110c8379e87a3b36a04ec6a3ce87ddb9960
Andrea Censi <andrea@cds.caltech.edu>
2014-02-06 16:02:50 -0500

    removed other packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='LatexSymbolManager',
         author="Andrea Censi",
         author_email="censi@mit.edu",
-        version="0.5",
+        version="0.6",
         package_dir={'':'src'},
         packages=find_packages('src'),
         entry_points={
@@ -16,17 +16,6 @@ setup(name='LatexSymbolManager',
            #'lsm_select = latex_symbol_manager.programs.select_subset:main',
            'lsm_nomenc = latex_symbol_manager.programs.nomenc:main',
            'lsm_collect = latex_symbol_manager.programs.collect.collect:main',
-
-           # Depednencies
-           'tex-deps = latex_deps.tex_deps:tex_deps_main',
-           'tex-deps-all = latex_deps.tex_deps_all:tex_deps_all_main',
-           'lyx-deps = latex_deps.lyx_deps:lyx_deps_main',
-           'lyx-deps-all = latex_deps.lyx_deps_all:lyx_deps_all_main',
-
-           # Summaries
-           'lyx-gen-chapter = lyx_sections:generate_chapter_contents_main',
-           'lyx-gen-part  = lyx_sections:generate_part_contents_main',
-           'lyx-gen  = lyx_sections:lyx_gen_main',
            ]
         },
         install_requires=['LaTeXGen', 'pyyaml'],
--- src/lyx_sections/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = '1.1'
-
-from .misc_utils import *
-from .subst import *
-from .generate_generic_templates import *
-from .generate_generic import *
-from .generate_chapter_contents import *
-from .generate_part_contents import *
-from .generic_lyx_gen import *
