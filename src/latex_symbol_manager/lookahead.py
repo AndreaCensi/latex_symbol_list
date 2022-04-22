593c899aebb77b3a5b14b74e696662c3ad2c32f3
Andrea Censi <acensi@ethz.ch>
2022-04-22 12:56:54 +0200

    [ci skip] works

--- src/latex_symbol_manager/lookahead.py
+++ src/latex_symbol_manager/lookahead.py
@@ -1,18 +1,28 @@
-class Lookahead:
-    def __init__(self, iterator):
+from typing import Generic, Iterator, List, Optional, TypeVar
+
+X = TypeVar("X")
+
+__all__ = ["Lookahead"]
+
+
+class Lookahead(Generic[X]):
+    iter: Iterator[X]
+    buffer: List[X]
+
+    def __init__(self, iterator: Iterator[X]):
         self.iter = iterator
         self.buffer = []
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[X]:
         return self
 
-    def __next__(self):  # @ReservedAssignment
+    def __next__(self) -> X:  # @ReservedAssignment
         if self.buffer:
             return self.buffer.pop(0)
         else:
             return next(self.iter)
 
-    def lookahead(self, n):
+    def lookahead(self, n: int) -> Optional[X]:
         """Return an item n entries ahead in the iteration."""
         while n >= len(self.buffer):
             try:
